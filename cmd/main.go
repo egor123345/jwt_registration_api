e8f600e2f1cfe4542faf3afb85675bbcf54c7597
Егоров Александр <egor123345@yandex.ru>
2023-03-13 03:55:51 +0300

    Добавлен config и чтение через viper

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"fmt"
 	postgres_client "jwt_registration_api/pkg/client/postgres"
+	"os"
+
+	"github.com/spf13/viper"
 
 	"github.com/sirupsen/logrus"
 )
 
-func main() {
-	fmt.Print("start")
+const configPath = "config/app_conf.yaml"
 
+func main() {
 	logger := logrus.New()
-	db, err := postgres_client.NewClient("Из гуся")
+
+	viper.SetConfigFile(configPath)
+	if err := viper.ReadInConfig(); err != nil {
+		logger.Fatal(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = viper.GetString("defaultPort")
+	}
+
+	db, err := postgres_client.NewClient(viper.GetString("db.url"))
 	if err != nil {
 		logger.Fatal(err)
 	}
